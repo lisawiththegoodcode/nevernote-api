318fbe13f3870a5aacba9b9d4a28482fbb5c620d
lisa umhey <lisa.umhey@gmail.com>
2019-11-02 19:18:26 -0500

    init

---
+++ src/main/java/com/nevernote/api/nevernoteapi/NevernoteApplication.java
@@ -0,0 +1,13 @@
+package com.nevernote.api.nevernoteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NevernoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NevernoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nevernote/api/nevernoteapi/NevernoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nevernote.api.nevernoteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NevernoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
